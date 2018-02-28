ef7875273d67bc8e5828a2717aa96469320b64b7
Saul van der Walt <saul@grimstack.io>
2018-02-28 22:02:41 +0200

    calculator example refactoring

--- examples/assets/calculator.js
+++ examples/assets/calculator.js
@@ -3,7 +3,7 @@ const {section, main, button} = dom
 
 const C = model({
   preOperator: true,
-  view: '0',
+  view: 0,
   reset () {
     C.view = 0
     C.left = C.right = ''
@@ -12,15 +12,15 @@ const C = model({
   }
 })
 
-const calculate = (num, operation, num1) => {
-  num = parseFloat(num)
-  num1 = parseFloat(num1)
+const calculate = (left, operation, right) => {
+  left = parseFloat(left)
+  right = parseFloat(right)
   const is = x => x === operation
   return (
-    is('/') && !(num === 0 && num1 === 0) ? num / num1 :
-    is('*') ? num * num1 :
-    is('+') ? num + num1 :
-    is('-') ? num - num1 :
+    is('/') && !(left === 0 && right === 0) ? left / right :
+    is('*') ? left * right :
+    is('+') ? left + right :
+    is('-') ? left - right :
     0
   )
 }
@@ -28,61 +28,42 @@ const calculate = (num, operation, num1) => {
 C.on.number(n => {
   const side = C.preOperator ? 'left' : 'right'
   if (isEmpty(C[side])) C[side] = ''
-  if (n === '.' || isNum(parseFloat(n))) C.view = C[side] += n
+  if ((n === '.' && !C[side].includes('.')) || isNum(parseFloat(n))) C.view = C[side] += n
 })
 
 C.on.operator(o => {
   if (C.preOperator) {
     C.operator = o
     C.preOperator = false
-  } else if (o === '=') {
+  } else if (o === '=' && C.operator) {
     C.view = C.left = calculate(C.left, C.operator, C.right)
     C.operator = C.right = ''
     C.preOperator = true
   }
+  if (C.activeOp) Class(C.activeOp, 'active', false)
+  Class(C.activeOp = view.operators[o], 'active', true)
 })
 
-const view = {keys: [], operators: new Map()}
-
+const view = {keys: [], operators: {}}
 view.body = main({$: 'body', class: 'calculator'})
-
-view.screen = section(
-  {$: view.body, class: 'screen'},
-  C.sync.view()
-)
-
+view.screen = section({$: view.body, class: 'screen'}, C.sync.view())
 view.numpad = section({
   $: view.body,
   class: 'numpad',
   onclick ({target}) {
-    if (view.keys.includes(target)) {
-      const key = view.keys.indexOf(target)
-      C.emit.number(key)
-    } else if (view.decimal === target) {
-      C.emit.number('.')
-    } else if (view.operators.has(target) && C.activeOp !== target) {
-      C.emit.operator(view.operators.get(target))
-      if (C.activeOp) Class(C.activeOp, 'active', false)
-      Class(C.activeOp = target, 'active', true)
-    } else if (view.clear === target) {
-      C.reset()
-    }
+    if (C.activeOp !== target && target.textContent in view.operators) {
+      C.emit.operator(target.textContent)
+    } else if (view.clear === target) return C.reset()
+    const num = (view.decimal === target && '.') || view.keys.indexOf(target)
+    if (num !== -1) C.emit.number(num)
   }
 })
-
 view.clear = button({$: view.numpad, class: 'clear'}, 'C')
-const numkey = i => {
-  view.keys[i] = button({$: view.numpad}, i)
-}
-const opkey = op => view.operators.set(button({$: view.numpad}, op), op)
 
-each([7, 8, 9], numkey)
-opkey('+')
-each([4, 5, 6], numkey)
-opkey('-')
-each([1, 2, 3], numkey)
-opkey('/')
-numkey(0)
-view.decimal = button({$: view.numpad}, '.')
-opkey('=')
-opkey('*')
+const numkey = i => { view.keys[i] = button({$: view.numpad}, i) }
+const opkey = op => { view.operators[op] = button({$: view.numpad}, op) }
+each([7, 8, 9], numkey); opkey('+')
+each([4, 5, 6], numkey); opkey('-')
+each([1, 2, 3], numkey); opkey('/')
+numkey(0); view.decimal = button({$: view.numpad}, '.')
+opkey('='); opkey('*')
