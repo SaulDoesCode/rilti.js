d3f95b3ef8c9d8ce67ac252c69005bc155e6ecf6
Saul van der Walt <saul@grimstack.io>
2018-03-10 13:33:05 +0200

    using express for dev server

--- init.js
+++ init.js
@@ -1,3 +1,4 @@
+const express = require('express')
 const http = require('http')
 const path = require('path')
 const url = require('url')
@@ -14,9 +15,6 @@ const exec = cmd => {
     })
   })
 }
-
-const UTF8 = 'utf8'
-
 const formatBytes = (bytes, decimals = 2) => {
   if (bytes === 0) return '0 Bytes'
   const k = 1000
@@ -24,19 +22,40 @@ const formatBytes = (bytes, decimals = 2) => {
   return (bytes / Math.pow(k, i)).toPrecision(decimals + 1) + ' ' + 'Bytes,KB,MB,GB,TB,PB,EB,ZB,YB'.split(',')[i]
 }
 
-const fileCache = {}
 
-const minfiyScript = (filename, minfile, webloc) => {
-  const rawCode = fs.readFileSync(filename, UTF8)
+const app = express()
 
-  if (rawCode === fileCache[webloc]) return
-  fileCache[webloc] = rawCode
+app.get('/', (req, res) => {
+  res.redirect('/docs/')
+})
 
-  const {code} = babel.transform(rawCode, {
-    sourceMaps: false,
-    minified: true,
-    presets: ['minify']
-  })
+app.use(express.static('./', {
+  dotfiles: 'ignore',
+  maxAge: 0,
+  redirect: true,
+  setHeaders (res, path, stat) {
+    res.set('Access-Control-Allow-Origin', '*')
+    res.set('Access-Control-Allow-Methods', 'GET, POST')
+    res.set('Access-Control-Allow-Headers', 'X-Requested-With,content-type')
+    res.set('Access-Control-Allow-Credentials', true)
+  }
+}))
+
+const PORT = 2018
+app.listen(PORT, () => console.log(`Server Started on at localhost:${PORT}/`))
+
+const minfiyScript = (filename, minfile) => {
+  const rawCode = fs.readFileSync(filename, 'utf8')
+  try {
+    var {code} = babel.transform(rawCode, {
+      sourceMaps: false,
+      minified: true,
+      presets: ['minify']
+    })
+  } catch (e) {
+    console.error(e)
+  }
+
 
   fs.writeFile(minfile, code, err => {
     if (err) throw err
@@ -56,22 +75,6 @@ const minfiyScript = (filename, minfile, webloc) => {
   })
 }
 
-const onlyOncePerN = (fn, n = 800, canRun = true) => (...args) => {
-  if (canRun) {
-    fn(...args)
-    canRun = false
-  }
-  setTimeout(() => {canRun = true}, n)
-}
-
-const urlify = location => {
-  const hasDot = location[0] === '.'
-  location = path.relative('./', path.resolve(location)).split('//').join('/')
-  if (location[0] != '/') location = '/' + location
-  if (hasDot) location = '.' + location
-  return location
-}
-
 const watcher = chokidar.watch('../rilti.js/', {
   ignored:  [
     '../rilti.js/node_modules',
@@ -80,23 +83,16 @@ const watcher = chokidar.watch('../rilti.js/', {
 })
 
 const fileMutation = location => {
-  const webloc = urlify(location)
-  console.log(webloc, ' changed')
+  console.log(`File Change: ${location}`)
   if (location.includes('/src/') && location.includes('.js')) {
     const filename = path.basename(location)
     const fileLoc = `./src/${filename}`
-    const minname = filename.split('.').map(e => e === 'js' ? 'min.js' : e).join('.')
+    const minname = filename.replace('.js', '.min.js')
     const minfileLoc = `./dist/${minname}`
     try {
-      setTimeout(minfiyScript, 25, fileLoc, minfileLoc, webloc)
+      setTimeout(minfiyScript, 25, fileLoc, minfileLoc)
     } catch (err) {
       console.log(`something's fishy: `, err)
-    }
-  } else {
-    try {
-      fileCache[webloc] = fs.readFileSync(location, UTF8)
-    } catch (e) {
-      console.error('touble reading: ' + webloc)
     }
   }
 }
@@ -108,102 +104,9 @@ watcher.on('ready', () => {
   watcher
   .on('add', fileMutation)
   .on('unlink', location => {
-    location = urlify(location)
-    delete fileCache[location]
     console.log(`removed: ${location}`)
   })
 })
-
-const mime_types = {
-  '.js': 'text/javascript',
-  '.json': 'application/json',
-  '.css': 'text/css',
-  '.png': 'image/png',
-  '.jpg': 'image/jpg',
-  '.wav': 'audio/wav'
-}
-
-const send = (res, code, data, type = 'text/html') => {
-  res.statusCode = code
-  res.setHeader('Access-Control-Allow-Origin', '*')
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type')
-  res.setHeader('Access-Control-Allow-Credentials', true)
-  if (data.constructor === Object) {
-    res.setHeader('Content-Type', 'application/json')
-    res.end(JSON.stringify(data))
-  } else {
-    res.setHeader('Content-Type', type)
-    res.end(data)
-  }
-}
-
-const PORT = 2018
-
-console.log(`Server Started on at localhost:${PORT}/`)
-http.createServer((req, res) => {
-    const pathname = url.parse(req.url).pathname
-    if (pathname === '/') {
-      res.writeHead(302, {Location: '/docs/'})
-      res.end()
-      return
-    }
-
-    let location = '.' + pathname
-
-    let ext = path.extname(location)
-    const type = mime_types[ext] || 'text/html'
-
-    if (!ext && location[location.length - 1] !== '/') {
-      res.writeHead(302, {Location: location + '/'})
-      res.end()
-      return
-    }
-
-    const data = fileCache[location]
-    if (data !== undefined && data.length) {
-      send(res, 200, data, type)
-      return
-    }
-
-    fs.exists(location, exists => {
-      if (!exists) {
-        send(res, 404, {
-          code: 404,
-          msg: `Couldn't find: ${location}`
-        })
-        return
-      }
-
-      if (fs.statSync(location).isDirectory()) {
-        const oldLoc = location
-        location = urlify(location + '/index.html')
-        if (location in fileCache && !(oldLoc in fileCache)) {
-          Object.defineProperty(fileCache, oldLoc, {
-            get() {
-              try {
-                return fileCache[location] || (fileCache[location] = fs.readFileSync(location, UTF8))
-              } catch (e) {}
-            }
-          })
-        }
-      }
-
-      fs.readFile(location, (err, data) => {
-        if (err) {
-          send(res, 500, `
-            <h1 style="color:red;">
-              Server Error: ${err.code}...
-            </h1>
-          `)
-          return
-        }
-        send(res, 200, data, type)
-        fileCache[location] = data
-      })
-    })
-})
-.listen(PORT)
 
 console.log(`
   Listening for file changes...
