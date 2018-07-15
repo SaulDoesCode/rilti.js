f35fb59d3e5481342a77b857c62c15e0039a14d0
Saul van der Walt <saulvdw@gmail.com>
2018-07-15 15:46:36 +0200

    matches codepen

--- examples/color-picker/modules/range-input.js
+++ examples/color-picker/modules/range-input.js
@@ -1,36 +1,56 @@
 import * as rilti from '../../../src/core.js'
-const {dom, component, on, once, each, isNum} = rilti
+const {dom, component, each, on, once, isNum, run} = rilti
 
-export const clamp = (n, min, max) => Math.min(Math.max(n, min), max)
+// keep a number between a minimum and maximum ammount
+const clamp = (n, min, max) => Math.min(Math.max(n, min), max)
 
-export const rangeInput = component('range-input', {
-  create (range) {
+// define element behavior
+component('range-input', {
+  // set up everything before element touches DOM
+  create (range /* Proxy<Function => Element> */) {
+    // setup zero values in state (observer-like abstraction tracking changes)
     range.state({value: 0, valueX: 0, valueY: 0})
+
+    // local vars for easier logic
+    let Value, ValueY
+
+    // create element <div class="handle"> and append to <range-input>
+    // also add property to range and get it as a const
     const handle = range.handle = dom.div.handle({$: range})
 
+    // set the range limits at 0-100% by default for X and Y axis
     if (range.limit == null) range.limitX = range.limit = 100
     if (range.limit !== range.limitX) range.limitX = range.limit
     if (range.limitY == null) range.limitY = range.limit
-    if (range.lockY == null) range.lockY = true
 
+    // set the X position by percentage/range number,
+    // move the handle accordingly and change state
     range.setX = (value = range.value || 0, skipChecks) => {
       if (!skipChecks && value === Value) return
       if (value > range.limitX || value < 0) throw new Error('value out of range')
+
+      // if the element is not in the dom
+      // then wait for it to mount first
       if (!range.mounted) {
         range.once.mount(e => range.setX(value))
         return
       }
+
+      // allow float values or round it to ints by default
       if (!range.decimals) value = Math.round(value)
 
       const hWidth = handle.offsetWidth
+      // get pixel range
       const Min = hWidth / 2
       const Max = range.offsetWidth - Min
+      // calculate pixel postion from range value
       const hLeft = (value / range.limitX) * (Max - Min)
       handle.style.left = hLeft + 'px'
-
+      // update all the states
       Value = range.state.value = range.state.valueX = value
     }
 
+    // same as setX but for Y axis
     range.setY = (value = range.valueY || 0, skipChecks) => {
       if (!skipChecks && value === Value) return
       if (value > range.limitY || value < 0) throw new Error('value out of range')
@@ -48,39 +68,50 @@ export const rangeInput = component('range-input', {
       ValueY = range.state.valueY = value
     }
 
-    let Value, ValueY
-
-    Object.defineProperties(range(), {
+    // get the raw Element/Node and define (s/g)etters
+    Object.defineProperties(range() /* -> <range-input> */, {
       value: {get: () => Value, set: range.setX},
       valueX: {get: () => Value, set: range.setX},
       valueY: {get: () => ValueY, set: range.setY}
     })
 
-    let rWidth
-    let rHeight
-    let rRect
+    let rWidth // range.offsetWidth
+    let rHeight // range.offsetHeight
+    let rRect // cache of range.getBoundingClientRect()
+    // called when user moves the handle
     const move = (x = 0, y = 0) => {
+      // check the the axis is not locked
+      // for when you want to use range-input as a slider
       if (!range.lockX) {
+        // adjust for relative position
         if (x < rRect.left) x = rRect.left
         else if (x > rRect.left + rWidth) x = rRect.left + rWidth
         x -= rRect.left
 
         const hWidth = handle.offsetWidth
+
+        // get pixel range
         const min = hWidth / 2
         const max = rWidth - min
 
+        // keep it inside the block
         const hLeft = clamp(x, min, max) - min
         handle.style.left = hLeft + 'px'
 
-        // percentage or range value
+        // pixel position -> percentage/value
         let value = (hLeft * range.limitX) / (max - min)
+
+        // round value to an int by default
         if (!range.decimals) value = Math.round(value)
+
+        // set it if it's not the same as the old value
         if (value !== Value) {
           Value = range.state.value = range.state.valueX = value
         }
       }
 
-      if (!range.lockY) {
+      // now do below as above for Y axis
+      if (!range.lockY) { // when it's not locked
         if (y < rRect.top) y = rRect.top
         else if (y > rRect.top + rWidth) y = rRect.top + rHeight
         y -= rRect.top
@@ -98,16 +129,22 @@ export const rangeInput = component('range-input', {
         }
       }
 
-      range.dispatchEvent(new window.CustomEvent('input'))
+      // .dispatchEvent(new CustomEvent('input'))
+      range.emit('input')
+      // call an update function if it's present as a prop
       if (range.update) range.update(range, handle)
     }
 
+    // track and manage starting, stopping and moving events
+    // for .pointer(up/down/move) event types respectively.
     const events = range.state.events = {
       move: on.pointermove(document, e => move(e.x, e.y)).off(),
+
       stop: on.pointerup(document, () => {
         events.move.off()
         events.start.on()
       }).off(),
+
       start: once.pointerdown([range, handle], () => {
         [rWidth, rHeight] = [range.offsetWidth, range.offsetHeight]
         rRect = range.getBoundingClientRect()
@@ -115,32 +152,50 @@ export const rangeInput = component('range-input', {
         events.stop.on()
       }).off()
     }
+    //    ^-- all the events are off at the start
+    //        they get turned on when the element mounts
   },
+
+  // when Element enters DOM set the positions
   mount (range) {
     if (!range.lockY) range.handle.style.top = 0
-    range.setX(); range.setY()
+    range.setX()
+    range.setY()
+    // start listening for user interactions
     range.state.events.start.on()
   },
+
+  // start listening again on DOM re-entry
   remount (range) {
     range.state.events.start.on()
   },
+
+  // stop listening when removed from DOM
   unmount ({state: {events}}) { each(events, e => e.off()) },
+
+  // track custom attribute to set some props conveniently
   attr: {
     opts (range, val) {
-      val.split(';')
-        .filter(v => v != null && v.length)
-        .map(pair => pair.trim().split(':').map(part => part.trim()))
-        .forEach(([prop, value]) => {
-          if (value.toLowerCase() === 'true') value = true
-          else if (value.toLowerCase() === 'false') value = false
-          else {
-            const temp = Number(value)
-            if (isNum(temp)) value = temp
-          }
-          if (prop === 'x' || prop === 'v') prop = 'valueX'
-          else if (prop === 'y') prop = 'valueY'
-          range[prop] = value
-        })
+      run(() => // wait for DOMContentLoaded first
+        val.split(';')
+          .filter(v => v != null && v.length)
+          .map(pair => pair.trim().split(':').map(part => part.trim()))
+          .forEach(([prop, value]) => {
+            if (value.toLowerCase() === 'true') value = true
+            else if (value.toLowerCase() === 'false') value = false
+            else {
+              const temp = Number(value)
+              if (isNum(temp)) value = temp
+            }
+            if (prop === 'x' || prop === 'v') {
+              range.setX(value, true)
+            } else if (prop === 'y') {
+              range.setY(value, true)
+            } else {
+              range[prop] = value
+            }
+          })
+      )
     }
   }
 })
