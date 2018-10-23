3fadf9e25a7d3134966ddae521317141bcf6f9af
Saul van der Walt <saulvdw@gmail.com>
2018-10-23 21:34:11 +0200

    doodling

--- examples/basics/main.js
+++ examples/basics/main.js
@@ -1,25 +1,51 @@
-const {dom, component, isFunc, isNode, isRenderable, isProxyNode} = rilti
-const {div, button} = rilti.dom
+/* global rilti */
+const {dom, databind, component, isFunc, isNode, isRenderable, isProxyNode} = rilti
+const {div, button} = dom
 
 component('card-component', {
-  state: {name: 'Saul'},
+  bind: {
+    name: {
+      key: 'innerText',
+      val: 'There',
+      change: val => `Hello ${val}!`
+    },
+    toggle: {
+      val: false,
+      change (val, {host}) {
+        host.class.toggle = val
+      }
+    }
+  },
   create (el) {
-    el.html = el.state`Hello ${'name'}!`
+    console.log('eg')
+    el.on.mouseover(e => {
+      el.name = (el.toggle = !el.toggle) ? 'Saul' : 'Moon moo'
+    })
   }
 })
 
-component('counter-button', {
-  state: {count: 0},
-  on: {
-    click: (e, {state}) => ++state.count
-  },
-  create (el) {
-    el.css = {
-      position: 'relative',
-      display: 'block',
-      margin: '5px',
-      padding: '5px 10px'
+{
+  const {component, componentReady, dom: {h1}, $} = rilti
+
+  component('counter-button', {
+    bind: {
+      count: {
+        key: 'innerText',
+        val: 0,
+        view: count => `clicks: ${count}`
+      }
+    },
+    on: {
+      click: (e, el) => ++el.count
     }
-    el.html = el.state`clicks: ${'count'}`
-  }
-})
+  })
+
+  componentReady('body > counter-button', el => {
+    const tellEm = h1['tell-em'](`You just won't stop clicking huh?`)
+    el.$count.observe(count => {
+      if (count > 20 && count < 40 && !tellEm.mounted) tellEm.render('body')
+      else if (count > 40 && count < 100) tellEm.txt += ' Seriously? '
+      else if (count > 100) tellEm.txt = 'What? You want a prize or something?'
+    })
+  })
+}
